dd439d81c789d4f08d9b1e8abeab1fe66862295b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 17:19:26 +0000

    Adding tarfile member sanitization to extractall()

--- hp_transfer_benchmarks/fcnet/download.py
+++ hp_transfer_benchmarks/fcnet/download.py
@@ -22,4 +22,26 @@ print("Starting the download. This might take a few minutes.")
 urllib.request.urlretrieve(DATA_URL, tar_file)
 
 with tarfile.open(tar_file) as tar_stream:
-    tar_stream.extractall(path=args.output_dir)
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar_stream, path=args.output_dir)
